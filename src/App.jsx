7973a65b1201b5098db6e64e3621c502b55dc356
orcemaceski <maceskiorce@gmail.com>
2024-04-14 14:21:25 +0200

    Added 'Loading...' while fetching data from google sheets

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,43 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const fetchSheet = async () => {
-    fetch(`${import.meta.env.VITE_API_URL}`)
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error("Error fetching data:", error));
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchSheet();
   }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="App">
       <h1>Data from Google Sheets</h1>
       <div className="cards-container">
-        {data.map((item, index) => (
+        {data.map((item) => (
           <Card
-            key={index}
+            key={item.id} // assuming id is a unique identifier in your data
             title={item.Title}
             description={item.Description}
             author={item.Author}
